6063ffeb490a3e54ecaf7c41cdba84fbebe6615a
ishaq-p <sultan.ishaq.321@gmail.com>
2024-05-26 22:01:31 +0300

    weights saved as PMG and imported

--- CNN.cpp
+++ CNN.cpp
@@ -18,7 +18,7 @@ public:
 };
 
 
-Matrix readPgm(const std::string& filename){
+Matrix readPgm(const std::string& filename, const int maxVal){
     std::ifstream file(filename);
     if(!file.is_open()){
         std::cerr << "Unable to open file " << filename << std::endl;
@@ -40,15 +40,16 @@ Matrix readPgm(const std::string& filename){
 
     std::stringstream ss(line);            /*the line afte the commented lines of the file*/
     int cols, rows;
-    if(!(ss >> cols >> rows)){
+    if(!(ss >> rows >> cols)){
         std::cerr << "Invalid PGM file: " << filename << " - Unable to read image dimensions" << std::endl;
         exit(EXIT_FAILURE);
     }
+    std::cout << rows << cols << std::endl;
 
     int max_val;
     file >> max_val;                /*next line containing the max pixel value in the file*/
-    if(max_val!=255){
-        std::cerr << "Invalid PGM file: " << filename << " - Max value is not 255" << std::endl;
+    if(max_val!=maxVal){
+        std::cerr << "Invalid PGM file: " << filename << " - Max value is not" << maxVal << std::endl;
         exit(EXIT_FAILURE);
     }
 
@@ -85,10 +86,25 @@ std::vector<float> softmax(const std::vector<float>&  logits){
     return exp_vals;
 }
 
+Matrix addPadding(const Matrix& input, const int paddingSize){
+    Matrix paddedInput = Matrix(input.rows+paddingSize*2, input.cols+paddingSize*2);
+    for(int i=0; i<input.rows+paddingSize*2; ++i){                   /*since we have to apply two extra lines to each side, hence we have to multiply to 2*/
+        for(int ii=0; ii<input.cols+paddingSize*2; ++ii){
+            if((i>1 && ii>1) && (i<input.rows && ii<input.cols)){
+                paddedInput.data[i][ii] = input.data[i][ii];
+            }else{
+                paddedInput.data[i][ii] = 0;
+            }
+        }
+    }
+    return paddedInput;
+}
 
-Matrix conLayer(const Matrix& input, const Matrix& kernal){
-    int outRows = input.rows - kernal.rows +1;
-    int outCols = input.cols - kernal.cols +1;
+
+Matrix conLayer(const Matrix& input, const Matrix& kernal){     /* KernalSize(5,5),  stride=1, padding=(2,2)*/
+    Matrix paddedInput = addPadding(input, 2);
+    int outRows = paddedInput.rows - kernal.rows +1;
+    int outCols = paddedInput.cols - kernal.cols +1;
     Matrix output(outRows, outCols);
 
     for(int i=0; i<outRows; ++i){
@@ -96,17 +112,17 @@ Matrix conLayer(const Matrix& input, const Matrix& kernal){
             float sum = 0.0f;
             for(int ki=0; ki<kernal.rows; ++ki){
                 for(int kj=0; kj<kernal.cols; ++kj){
-                    sum += input.data[i + ki][j + kj] * kernal.data[ki][kj];
+                    sum += paddedInput.data[i + ki][j + kj] * kernal.data[ki][kj];
                 }
             }
-            output.data[i][j] = relu(sum);
+            output.data[i][j] = relu(sum);                        /* ReLU function is applied here*/
         }
     }
     return output;
 }
 
 
-Matrix maxPooling(const Matrix& input, int poolSize){
+Matrix maxPooling(const Matrix& input, int poolSize){    /* KernalSize(2,2),  stride=2, padding=(0,0)*/
     int outRows = input.rows / poolSize;
     int outCols = input.cols / poolSize;
     Matrix output(outRows, outCols);
@@ -136,32 +152,37 @@ public:
     int filterSize;
     std::vector<Matrix> filters;
 
-    ConLayer(int numFilters, int filterSize) : numFilters(numFilters), filterSize(filterSize){
+    ConLayer(int numFilters, int filterSize, int cnn_layerNum, std::vector<Matrix>& weights) : numFilters(numFilters), filterSize(filterSize){
         filters.resize(numFilters, Matrix(filterSize, filterSize));
         for(int filNum=0; filNum<numFilters; ++filNum){
             for(int i=0; i<filterSize; ++i){
                 for(int ii=0; ii<filterSize; ++ii){
+
+                    if(cnn_layerNum==0){
+                        filters[filNum].data[i][ii] = weights[0].data[filNum][ii + 5*i];
+                    }else{
                     filters[filNum].data[i][ii] = static_cast<float>(rand() %10)/10;
+                    }
                 }
             }
         }
     }
 
-    // Matrix forward(const Matrix& input){
     std::vector<Matrix> forward(const Matrix& input){
         std::vector<Matrix> finalOutput(numFilters, Matrix(input.rows, input.cols));
         for (int i=0; i<numFilters; ++i){
-            finalOutput[i] = conLayer(input, filters[i]);   /*ReLU is applied inside this function*/
-        }
-        for (int i=0; i<numFilters; ++i){
-            finalOutput[i] = maxPooling(finalOutput[i], 2);
+            Matrix conLayer_out = conLayer(input, filters[i]);   /*ReLU is applied inside this function*/
+            finalOutput[i] = maxPooling(conLayer_out, 2);
         }
         return finalOutput;
-
-        // Matrix out = conLayer(input, filters[0]);
-        // out = maxPooling(out, 2);
-        // return out;
     }
+
+    // void storeWeights(std::vector<Matrix>& weights){
+    //     for(size_t i; i<filters.size(); ++i){
+
+    //     }
+
+    // }
 };
 
 
@@ -170,11 +191,13 @@ public:
     Matrix weights;
     std::vector<float> biases;
 
-    FClayer(int inputSize, int outSize) : weights(inputSize, outSize), biases(outSize){
+    FClayer(int inputSize, int outSize, const Matrix& inWeights) : weights(inputSize, outSize), biases(outSize){
         for(int i=0; i<outSize; ++i){
             biases[i] = static_cast<float>(rand() %10)/10;                     /*assigning random numbers to the weights of fullyConnected Layer*/
             for(int ii=0; ii<inputSize; ++ii){
-                weights.data[ii][i] = static_cast<float>(rand() %20)/10 - 1;    /*assigning random numbers to the weights of fullyConnected Layer*/
+
+                // !!!!!Note!!!! recheck this
+                weights.data[ii][i] = inWeights.data[i][ii];    /*assigning random numbers to the weights of fullyConnected Layer*/
             }
         }
     }
@@ -195,63 +218,64 @@ public:
 
 int main(){
     const int numFilter1=16;
-    const int numFilter2=2;
+    const int numFilter2=32;
     const int filterSize=5;
     srand((unsigned) time(NULL));
+    std::vector<Matrix> cnn1Weight_pack(1, Matrix(16,25));
+    std::vector<Matrix> cnn2Weight_pack(32, Matrix(16,25));
+
+    const Matrix cnn1_weight = readPgm("modelWeights/cnn1/cnn1_weights.pmg", 1000000);
+    cnn1Weight_pack[0] = cnn1_weight;
+
+    const Matrix fc1_weight = readPgm("modelWeights/fc1/fc1_weights.pmg", 1000000);
+
+    for(int i=0; i<32; ++i){
+        std::string filename = "modelWeights/cnn2/cnn2_weights"+ std::to_string(i) +".pmg";
+        cnn2Weight_pack[i] = readPgm(filename, 1000000);
+    }
+
 
     std::string filename = "test.pmg";
-    Matrix img = readPgm(filename);
+    Matrix img = readPgm(filename, 255);
     std::cout << "Read PGM image of size" << img.rows << "x" << img.cols << std::endl;
 
-    ConLayer convul1 = ConLayer(numFilter1, filterSize);
+    ConLayer convul1 = ConLayer(numFilter1, filterSize, 0, cnn1Weight_pack);
     std::vector<Matrix> conImg = convul1.forward(img);
 
     std::vector<Matrix> conImg_final(numFilter1*numFilter2, Matrix(1, 1));
-    ConLayer convul2 = ConLayer(numFilter2, filterSize);
+    ConLayer convul2 = ConLayer(numFilter2, filterSize, 1, cnn2Weight_pack);
     for(int i=0; i<numFilter1; ++i){
         std::vector<Matrix> output = convul2.forward(conImg[i]);
         conImg_final[0+ 2*i] = output[0];
         conImg_final[1+ 2*i] = output[1];
-        // std::cout << 0+2*i << " " << conImg_final[0+ 2*i].data[0][0] << "\t\t" << 1+2*i << " " << conImg_final[1+ 2*i].data[0][0] << std::endl ;
+        // std::cout << 0+2*i << " " << conImg_final[0+ 2*i].rows << "\t\t" << 1+2*i << " " << conImg_final[1+ 2*i].cols << std::endl ;
     }
 
-    // for(int i=0; i<numFilter1*numFilter2; ++i){
-    //     std::cout << conImg_final[i].data[0][0] << std::endl;
-    // }
 
+    const int fcInput = conImg_final[0].cols * conImg_final[0].rows * numFilter2;
 
-
-    const int fcInput = conImg_final[0].cols * 32;
-
-    FClayer fconectd = FClayer(fcInput, 10);
+    FClayer fconectd = FClayer(fcInput, 10, fc1_weight);
 
     std::vector<float> flattenImg(fcInput);
-    // for(int fltrNum=0; fltrNum<numFilter1; ++fltrNum){
-    //     for(int i=0; i<conImg[fltrNum].rows; ++i){
-    //         for(int ii=0; ii<conImg[fltrNum].cols; ++ii){
-    //             int index=i*6+ii;
-    //             flattenImg[index] = conImg[fltrNum].data[i][ii];
-    //             std::cout << std::endl << index + 36*fltrNum << " " << conImg[fltrNum].data[i][ii] << " ";
-    //         }
-    //     }
-    //     std::cout << std::endl;
-    // }
-
-    for(int fltrNum=0; fltrNum<numFilter2*numFilter1; ++fltrNum){
+    for(int fltrNum=0; fltrNum<numFilter2; ++fltrNum){
         for(int i=0; i<conImg_final[fltrNum].rows; ++i){
             for(int ii=0; ii<conImg_final[fltrNum].cols; ++ii){
-                int index=i+ii;
+                int index=i*conImg_final[fltrNum].rows+ii;
                 flattenImg[index] = conImg_final[fltrNum].data[i][ii];
-                std::cout << std::endl << index + fltrNum << " " << conImg_final[fltrNum].data[i][ii] << " " << flattenImg[index] << " ";
+                // std::cout << std::endl << index + 16*fltrNum << " " << conImg_final[fltrNum].data[i][ii] << " ";
             }
         }
+        // std::cout << std::endl;
+        // break;
     }
+
     std::vector<float> fc_output = fconectd.forward(flattenImg);   
     std::vector<float> fc_output_soft = softmax(fc_output);
     std::cout <<  std::endl;
     for (int i=0; i<10; ++i){
         std::cout << i << "->" << fc_output_soft[i] << "\t";
     }
+
 
     return 0;
 } 
--- ToDo.md
+++ ToDo.md
@@ -2,9 +2,18 @@
 -> format the cpp code, make it in best form
 -> implement the same functionality in python first
 -> add the neccessary points to the cpp code
--> implement the first training 
+-> implement the first training                         **
 
 -> apply the double CNN layer
 -> simplify the code
 -> train the model with the same architeture with python
+
+## 26-05-2024
 -> import the weights and filters as PMG
+-> perform the first classification                    **
+-> improve the code format
+-> improve the dataStructures                         **
+
+## 26-05-2024
+-> storing the weights for the cnn2 filters     **
+-> compare some recursive with for-Loops        **
