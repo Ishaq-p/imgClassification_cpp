42d035235f1cd44b270e488a0089a7c706324470
ishaq-p <sultan.ishaq.321@gmail.com>
2024-05-29 00:41:53 +0300

    v1 -multithread

--- CNN2.cpp
+++ CNN2.cpp
@@ -1,10 +1,14 @@
+#include <thread>
+#include <mutex>
 #include <iostream>
 #include <vector>
 #include <cmath>
 #include <fstream>
 #include <sstream>
 #include <string>
-#include<cstdlib>
+#include <cstdlib>
+
+std::mutex mtx; // Mutex for protecting shared data
 
 
 class Matrix{
@@ -44,7 +48,6 @@ Matrix readPgm(const std::string& filename, const int maxVal){
         std::cerr << "Invalid PGM file: " << filename << " - Unable to read image dimensions" << std::endl;
         exit(EXIT_FAILURE);
     }
-    // std::cout << rows << "x"<< cols << std::endl;
 
     int max_val;
     file >> max_val;                /*next line containing the max pixel value in the file*/
@@ -69,7 +72,6 @@ Matrix readPgm(const std::string& filename, const int maxVal){
     return img;
 }
 
-
 float relu(float x){
     return std::max(0.0f, x);
 }
@@ -82,10 +84,7 @@ int final_softmax(const std::vector<float> softmax_out){
             class_=i;
         }
         classifiedClass = std::max(softmax_out[i], classifiedClass);
-
-        // std::cout << std::endl << std::max(softmax_out[i], classifiedClass) << std::endl; 
     }
-    // std::cout << std::endl << softmax_out.size() <<std::endl;
     return class_;
 }
 
@@ -115,7 +114,6 @@ Matrix addPadding(const Matrix& input, const int paddingSize){
     return paddedInput;
 }
 
-
 Matrix conLayer(const Matrix& input, const Matrix& kernal){     /* KernalSize(5,5),  stride=1, padding=(2,2)*/
     Matrix paddedInput = addPadding(input, 2);
     int outRows = paddedInput.rows - kernal.rows +1;
@@ -130,14 +128,11 @@ Matrix conLayer(const Matrix& input, const Matrix& kernal){     /* KernalSize(5,
                     sum += paddedInput.data[i + ki][j + kj] * kernal.data[ki][kj];
                 }
             }
-            // std::cout << sum << relu(sum) << ":khelo " << std::endl<<std::endl;
             output.data[i][j] = relu(sum);                        /* ReLU function is applied here*/
         }
     }
-    // std::cout << "khan";
     return output;
 }
-
 
 Matrix maxPooling(const Matrix& input, int poolSize){    /* KernalSize(2,2),  stride=2, padding=(0,0)*/
     int outRows = input.rows / poolSize;
@@ -234,15 +229,9 @@ public:
     }
 
     std::vector<float> forward(const std::vector<float>& input, std::vector<float>& fc_output){
-            // std::cout<< "pass61 "<< biases.size() << std::endl;
-        // std::vector<float> output;
-            // std::cout<< "pass62"<<std::endl;
-
         for(int i=0; i<biases.size(); ++i){
-            // std::cout << "pass62"<<std::endl;
             fc_output[i] = biases[i];
             for(int j=0; j<input.size(); ++j){
-
                 fc_output[i] += input[j] * weights.data[i][j];
             }
             fc_output[i] = relu(fc_output[i]);
@@ -251,6 +240,7 @@ public:
     }
 };
 
+
 void imgFlattener(const std::vector<Matrix>& conImg_final, std::vector<float>& flattenImg){
     for(int fltrNum=0; fltrNum<32; ++fltrNum){
         for(int i=0; i<conImg_final[fltrNum].rows; ++i){
@@ -258,12 +248,10 @@ void imgFlattener(const std::vector<Matrix>& conImg_final, std::vector<float>& f
                 
                 int index = ii + i * conImg_final[fltrNum].cols + fltrNum * (conImg_final[0].rows * conImg_final[0].cols);
                 flattenImg[index] = conImg_final[fltrNum].data[i][ii];
-                // std::cout << index << " ";
             }
         }
     }
 }
-
 
 int mini_main(const std::string& filename1){
     const int inSize=1;
@@ -283,16 +271,12 @@ int mini_main(const std::string& filename1){
         std::string filename = "modelWeights/cnn2/cnn2_weights"+ std::to_string(i) +".pmg";
         cnn2Weight_pack[i] = readPgm(filename, 1000000);
     }
-    // std::cout<< std::endl << "pass1"<<std::endl;
 
 
     std::string filename = "Data/finalData/trainData/"+filename1+".pmg";
     Matrix img = readPgm(filename, 255);
-    // std::cout << "Read PGM image of size" << img.rows << "x" << img.cols << std::endl;
 
-    // std::cout<< "pass1.5"<<std::endl;
     ConLayer convul1 = ConLayer(inSize, outSize1, filterSize, 0, cnn1Weight_pack);
-    // std::cout<< "pass2"<<std::endl;
 
     std::vector<Matrix> img1 = std::vector<Matrix>(1, Matrix(16,16));
     img1 = {img};
@@ -301,60 +285,58 @@ int mini_main(const std::string& filename1){
 
     std::vector<Matrix> conImg_final = std::vector<Matrix>(32, Matrix(4,4));
     ConLayer convul2 = ConLayer(outSize1, outSize2, filterSize, 1, cnn2Weight_pack);
-    // std::cout<< "pass3"<<std::endl;
 
     conImg_final = convul2.forward(conImg);
-
-    // std::cout<< "pass4"<<std::endl;
 
 
     const int fcInput = conImg_final[0].cols * conImg_final[0].rows * outSize2;
     FClayer fconectd = FClayer(fcInput, 10, fc1_weight, fc1_bias);
 
     std::vector<float> flattenImg = std::vector<float>(fcInput);
-        // std::cout<< "pass5"<<std::endl;
     imgFlattener(conImg_final, flattenImg);
-
-    // std::cout<< "pass6"<<std::endl;
-
-    // for(int i=0; i<flattenImg.size(); ++i){
-    //     std::cout << flattenImg[i]<<": ";
-    // }
 
     std::vector<float> fc_output(10);
     fconectd.forward(flattenImg, fc_output); 
-
-
-        // std::cout<< "pass7"<<std::endl;
-
     std::vector<float> fc_output_soft = softmax(fc_output);
-    // for (int i=0; i<10; ++i){
-    //     std::cout << i << "->" << fc_output_soft[i] << "\t";
-    // }
-    // std::cout<< "pass8"<<std::endl;
-
-    // std::cout << filename1 << " the number in the img is: " << final_softmax(fc_output_soft);
-        // std::cout <<  std::endl;
-
-
 
     return final_softmax(fc_output_soft);
 } 
 
-int main(){
-
-    std::vector<int> y = {5, 0, 4, 1, 9, 2, 1, 3, 1, 4, 3, 5, 3, 6, 1, 7, 2, 8, 6, 9, 4, 0, 9, 1, 1, 2, 4, 3, 2, 7, 3, 8, 6, 9, 0, 5, 6, 0, 7, 6, 1, 8, 7, 9, 3, 9, 8, 5, 9, 3, 3, 0, 7, 4, 9, 8, 0, 9, 4, 1, 4, 4, 6, 0, 4, 5, 6, 1, 0, 0, 1, 7, 1, 6, 3, 0, 2, 1, 1, 7, 9, 0, 2, 6, 7, 8, 3, 9, 0, 4, 6, 7, 4, 6, 8, 0, 7, 8, 3, 1, 5, 7, 1, 7, 1, 1, 6, 3, 0, 2, 9, 3, 1, 1, 0, 4, 9, 2, 0, 0, 2, 0, 2, 7, 1, 8, 6, 4, 1, 6, 3, 4, 5, 9, 1, 3, 3, 8, 5, 4, 7, 7, 4, 2, 8, 5, 8, 6, 7, 3, 4, 6, 1, 9, 9, 6, 0, 3, 7, 2, 8, 2, 9, 4, 4, 6, 4, 9, 7, 0, 9, 2, 9, 5, 1, 5, 9, 1, 2, 3, 2, 3, 5, 9, 1, 7, 6, 2, 8, 2, 2, 5, 0, 7, 4, 9, 7, 8, 3, 2, 1, 1, 8, 3, 6, 1, 0, 3, 1, 0, 0, 1, 7, 2, 7, 3, 0, 4, 6, 5, 2, 6, 4, 7, 1, 8, 9, 9, 3, 0, 7, 1, 0, 2, 0, 3, 5, 4, 6, 5, 8, 6, 3, 7, 5, 8, 0, 9, 1, 0, 3, 1, 2, 2, 3, 3, 6, 4, 7, 5, 0, 6, 2, 7, 9, 8, 5, 9, 2, 1, 1, 4, 4, 5, 6, 4, 1, 2, 5, 3, 9, 3, 9, 0, 5, 9, 6, 5, 7, 4, 1, 3, 4, 0, 4, 8, 0, 4, 3, 6, 8, 7, 6, 0, 9, 7, 5, 7, 2, 1, 1, 6, 8, 9, 4, 1, 5, 2, 2, 9, 0, 3, 9, 6, 7, 2, 0, 3, 5, 4, 3, 6, 5, 8, 9, 5, 4, 7, 4, 2, 7, 3, 4, 8, 9, 1, 9, 2, 8, 7, 9, 1, 8, 7, 4, 1, 3, 1, 1, 0, 2, 3, 9, 4, 9, 2, 1, 6, 8, 4, 7, 7, 4, 4, 9, 2, 5, 7, 2, 4, 4, 2, 1, 9, 7, 2, 8, 7, 6, 9, 2, 2, 3, 8, 1, 6, 5, 1, 1, 0, 2, 6, 4, 5, 8, 3, 1, 5, 1, 9, 2, 7, 4, 4, 4, 8, 1, 5, 8, 9, 5, 6, 7, 9, 9, 3, 7, 0, 9, 0, 6, 6, 2, 3, 9, 0, 7, 5, 4, 8, 0, 9, 4, 1, 2, 8, 7, 1, 2, 6, 1, 0, 3, 0, 1, 1, 8, 2, 0, 3, 9, 4, 0, 5, 0, 6, 1, 7, 7, 8, 1, 9, 2, 0, 5, 1, 2, 2, 7, 3, 5, 4, 9, 7, 1, 8, 3, 9, 6, 0, 3, 1, 1, 2, 6, 3, 5, 7, 6, 8, 3, 9, 5, 8, 5, 7, 6, 1, 1, 3, 1, 7, 5, 5, 5, 2, 5, 8, 7, 0, 9, 7, 7, 5, 0, 9, 0, 0, 8, 9, 2, 4, 8, 1, 6, 1, 6, 5, 1, 8, 3, 4, 0, 5, 5, 8, 3, 6, 2, 3, 9, 2, 1, 1, 5, 2, 1, 3, 2, 8, 7, 3, 7, 2, 4, 6, 9, 7, 2, 4, 2, 8, 1, 1, 3, 8, 4, 0, 6, 5, 9, 3, 0, 9, 2, 4, 7, 1, 2, 9, 4, 2, 6, 1, 8, 9, 0, 6, 6, 7, 9, 9, 8, 0, 1, 4, 4, 6, 7, 1, 5, 7, 0, 3, 5, 8, 4, 7, 1, 2, 5, 9, 5, 6, 7, 5, 9, 8, 8, 3, 6, 9, 7, 0, 7, 5, 7, 1, 1, 0, 7, 9, 2, 3, 7, 3, 2, 4, 1, 6, 2, 7, 5, 5, 7, 4, 0, 2, 6, 3, 6, 4, 0, 4, 2, 6, 0, 0, 0, 0, 3, 1, 6, 2, 2, 3, 1, 4, 1, 5, 4, 6, 4, 7, 2, 8, 7, 9, 2, 0, 5, 1, 4, 2, 8, 3, 2, 4, 1, 5, 4, 6, 0, 7, 9, 8, 4, 9, 8, 0, 1, 1, 0, 2, 2, 3, 2, 4, 4, 5, 8, 6, 5, 7, 7, 8, 8, 9, 7, 4, 7, 3, 2, 0, 8, 6, 8, 6, 1, 6, 8, 9, 4, 0, 9, 0, 4, 1, 5, 4, 7, 5, 3, 7, 4, 9, 8, 5, 8, 6, 3, 8, 6, 9, 9, 1, 8, 3, 5, 8, 6, 5, 9, 7, 2, 5, 0, 8, 5, 1, 1, 0, 9, 1, 8, 6, 7, 0, 9, 3, 0, 8, 8, 9, 6, 7, 8, 4, 7, 5, 9, 2, 6, 7, 4, 5, 9, 2, 3, 1, 6, 3, 9, 2, 2, 5, 6, 8, 0, 7, 7, 1, 9, 8, 7, 0, 9, 9, 4, 6, 2, 8, 5, 1, 4, 1, 5, 5, 1, 7, 3, 6, 4, 3, 2, 5, 6, 4, 4, 0, 4, 4, 6, 7, 2, 4, 3, 3, 8, 0, 0, 3, 2, 2, 9, 8, 2, 3, 7, 0, 1, 1, 0, 2, 3, 3, 8, 4, 3, 5, 7, 6, 4, 7, 7, 8, 5, 9, 7, 0, 3, 1, 6, 2, 4, 3, 4, 4, 7, 5, 9, 6, 9, 0, 7, 1, 4, 2, 7, 3, 6, 7, 5, 8, 4, 5, 5, 2, 7, 1, 1, 5, 6, 8, 5, 8, 4, 0, 7, 9, 9, 2, 9, 7, 7, 8, 7, 4, 2, 6, 9, 1, 7, 0, 6, 4, 2, 5, 7, 0, 7, 1, 0, 3, 7, 6, 5, 0, 6, 1, 5, 1, 7, 8, 5, 0, 3, 4, 7, 7, 5, 7, 8, 6, 9, 3, 8, 6, 1, 0, 9, 7, 1, 3, 0, 5, 6, 4, 4, 2, 4, 4, 3, 1, 7, 7, 6, 0, 3, 6};
-    int corrected=0;
-    float accuracy=0.0f;
-    for(int i=0; i<1000; ++i){
+void process_chunk(const std::vector<int> &y, int start, int end, int &corrected) {
+    int local_corrected = 0; // Local corrected count to avoid frequent locking
+    for (int i = start; i < end; ++i) {
         std::string filename = std::to_string(i);
         int yhat = mini_main(filename);
 
-        if(yhat==y[i]){
-            corrected +=1;
+        if (yhat == y[i]) {
+            ++local_corrected;
         }
-        // std::cout << "->" << y[i] << std::endl;
     }
-    std::cout << corrected << " the accuracy: "<< float(corrected)/float(y.size());
 
+    // Update the shared corrected count
+    std::lock_guard<std::mutex> lock(mtx);
+    corrected += local_corrected;
+}
+
+int main() {
+    std::vector<int> y = {5, 0, 4, 1, 9, 2, 1, 3, 1, 4, 3, 5, 3, 6, 1, 7, 2, 8, 6, 9, 4, 0, 9, 1, 1, 2, 4, 3, 2, 7, 3, 8, 6, 9, 0, 5, 6, 0, 7, 6, 1, 8, 7, 9, 3, 9, 8, 5, 9, 3, 3, 0, 7, 4, 9, 8, 0, 9, 4, 1, 4, 4, 6, 0, 4, 5, 6, 1, 0, 0, 1, 7, 1, 6, 3, 0, 2, 1, 1, 7, 9, 0, 2, 6, 7, 8, 3, 9, 0, 4, 6, 7, 4, 6, 8, 0, 7, 8, 3, 1, 5, 7, 1, 7, 1, 1, 6, 3, 0, 2, 9, 3, 1, 1, 0, 4, 9, 2, 0, 0, 2, 0, 2, 7, 1, 8, 6, 4, 1, 6, 3, 4, 5, 9, 1, 3, 3, 8, 5, 4, 7, 7, 4, 2, 8, 5, 8, 6, 7, 3, 4, 6, 1, 9, 9, 6, 0, 3, 7, 2, 8, 2, 9, 4, 4, 6, 4, 9, 7, 0, 9, 2, 9, 5, 1, 5, 9, 1, 2, 3, 2, 3, 5, 9, 1, 7, 6, 2, 8, 2, 2, 5, 0, 7, 4, 9, 7, 8, 3, 2, 1, 1, 8, 3, 6, 1, 0, 3, 1, 0, 0, 1, 7, 2, 7, 3, 0, 4, 6, 5, 2, 6, 4, 7, 1, 8, 9, 9, 3, 0, 7, 1, 0, 2, 0, 3, 5, 4, 6, 5, 8, 6, 3, 7, 5, 8, 0, 9, 1, 0, 3, 1, 2, 2, 3, 3, 6, 4, 7, 5, 0, 6, 2, 7, 9, 8, 5, 9, 2, 1, 1, 4, 4, 5, 6, 4, 1, 2, 5, 3, 9, 3, 9, 0, 5, 9, 6, 5, 7, 4, 1, 3, 4, 0, 4, 8, 0, 4, 3, 6, 8, 7, 6, 0, 9, 7, 5, 7, 2, 1, 1, 6, 8, 9, 4, 1, 5, 2, 2, 9, 0, 3, 9, 6, 7, 2, 0, 3, 5, 4, 3, 6, 5, 8, 9, 5, 4, 7, 4, 2, 7, 3, 4, 8, 9, 1, 9, 2, 8, 7, 9, 1, 8, 7, 4, 1, 3, 1, 1, 0, 2, 3, 9, 4, 9, 2, 1, 6, 8, 4, 7, 7, 4, 4, 9, 2, 5, 7, 2, 4, 4, 2, 1, 9, 7, 2, 8, 7, 6, 9, 2, 2, 3, 8, 1, 6, 5, 1, 1, 0, 2, 6, 4, 5, 8, 3, 1, 5, 1, 9, 2, 7, 4, 4, 4, 8, 1, 5, 8, 9, 5, 6, 7, 9, 9, 3, 7, 0, 9, 0, 6, 6, 2, 3, 9, 0, 7, 5, 4, 8, 0, 9, 4, 1, 2, 8, 7, 1, 2, 6, 1, 0, 3, 0, 1, 1, 8, 2, 0, 3, 9, 4, 0, 5, 0, 6, 1, 7, 7, 8, 1, 9, 2, 0, 5, 1, 2, 2, 7, 3, 5, 4, 9, 7, 1, 8, 3, 9, 6, 0, 3, 1, 1, 2, 6, 3, 5, 7, 6, 8, 3, 9, 5, 8, 5, 7, 6, 1, 1, 3, 1, 7, 5, 5, 5, 2, 5, 8, 7, 0, 9, 7, 7, 5, 0, 9, 0, 0, 8, 9, 2, 4, 8, 1, 6, 1, 6, 5, 1, 8, 3, 4, 0, 5, 5, 8, 3, 6, 2, 3, 9, 2, 1, 1, 5, 2, 1, 3, 2, 8, 7, 3, 7, 2, 4, 6, 9, 7, 2, 4, 2, 8, 1, 1, 3, 8, 4, 0, 6, 5, 9, 3, 0, 9, 2, 4, 7, 1, 2, 9, 4, 2, 6, 1, 8, 9, 0, 6, 6, 7, 9, 9, 8, 0, 1, 4, 4, 6, 7, 1, 5, 7, 0, 3, 5, 8, 4, 7, 1, 2, 5, 9, 5, 6, 7, 5, 9, 8, 8, 3, 6, 9, 7, 0, 7, 5, 7, 1, 1, 0, 7, 9, 2, 3, 7, 3, 2, 4, 1, 6, 2, 7, 5, 5, 7, 4, 0, 2, 6, 3, 6, 4, 0, 4, 2, 6, 0, 0, 0, 0, 3, 1, 6, 2, 2, 3, 1, 4, 1, 5, 4, 6, 4, 7, 2, 8, 7, 9, 2, 0, 5, 1, 4, 2, 8, 3, 2, 4, 1, 5, 4, 6, 0, 7, 9, 8, 4, 9, 8, 0, 1, 1, 0, 2, 2, 3, 2, 4, 4, 5, 8, 6, 5, 7, 7, 8, 8, 9, 7, 4, 7, 3, 2, 0, 8, 6, 8, 6, 1, 6, 8, 9, 4, 0, 9, 0, 4, 1, 5, 4, 7, 5, 3, 7, 4, 9, 8, 5, 8, 6, 3, 8, 6, 9, 9, 1, 8, 3, 5, 8, 6, 5, 9, 7, 2, 5, 0, 8, 5, 1, 1, 0, 9, 1, 8, 6, 7, 0, 9, 3, 0, 8, 8, 9, 6, 7, 8, 4, 7, 5, 9, 2, 6, 7, 4, 5, 9, 2, 3, 1, 6, 3, 9, 2, 2, 5, 6, 8, 0, 7, 7, 1, 9, 8, 7, 0, 9, 9, 4, 6, 2, 8, 5, 1, 4, 1, 5, 5, 1, 7, 3, 6, 4, 3, 2, 5, 6, 4, 4, 0, 4, 4, 6, 7, 2, 4, 3, 3, 8, 0, 0, 3, 2, 2, 9, 8, 2, 3, 7, 0, 1, 1, 0, 2, 3, 3, 8, 4, 3, 5, 7, 6, 4, 7, 7, 8, 5, 9, 7, 0, 3, 1, 6, 2, 4, 3, 4, 4, 7, 5, 9, 6, 9, 0, 7, 1, 4, 2, 7, 3, 6, 7, 5, 8, 4, 5, 5, 2, 7, 1, 1, 5, 6, 8, 5, 8, 4, 0, 7, 9, 9, 2, 9, 7, 7, 8, 7, 4, 2, 6, 9, 1, 7, 0, 6, 4, 2, 5, 7, 0, 7, 1, 0, 3, 7, 6, 5, 0, 6, 1, 5, 1, 7, 8, 5, 0, 3, 4, 7, 7, 5, 7, 8, 6, 9, 3, 8, 6, 1, 0, 9, 7, 1, 3, 0, 5, 6, 4, 4, 2, 4, 4, 3, 1, 7, 7, 6, 0, 3, 6};
+    int corrected = 0;
+    const int num_threads = std::thread::hardware_concurrency(); // Get the number of available threads
+    const int chunk_size = y.size() / num_threads;
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < num_threads; ++i) {
+        int start = i * chunk_size;
+        int end = (i == num_threads - 1) ? y.size() : start + chunk_size;
+        threads.emplace_back(process_chunk, std::ref(y), start, end, std::ref(corrected));
+    }
+
+    for (auto &t : threads) {
+        t.join(); // Wait for all threads to finish
+    }
+
+    float accuracy = float(corrected) / y.size();
+    std::cout << "The accuracy: " << accuracy << std::endl;
+
+    return 0;
 }
